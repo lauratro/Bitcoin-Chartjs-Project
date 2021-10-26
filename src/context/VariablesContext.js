03f498e23755b909f91e343ca7705bbd3bec9403
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 10:48:24 +0200

    starting date defined in the VariablesContext

--- src/context/VariablesContext.js
+++ src/context/VariablesContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 const initContextVariables = {
   startDate: "",
@@ -9,6 +9,20 @@ export const VariablesContext = createContext(initContextVariables);
 export const VariablesContextProvider = ({ children }) => {
   const [startDate, setStartDate] = useState(initContextVariables.startDate);
   const [finalDate, setFinalDate] = useState(initContextVariables.finalDate);
+
+  useEffect(() => {
+    let getCurrentDate = () => {
+      let newDate = new Date();
+      let date = newDate.getDate();
+      let month = newDate.getMonth() + 1;
+      let year = newDate.getFullYear();
+
+      return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`;
+    };
+    setStartDate(getCurrentDate());
+  }, []);
+
+  console.log(startDate);
 
   return (
     <VariablesContext.Provider
