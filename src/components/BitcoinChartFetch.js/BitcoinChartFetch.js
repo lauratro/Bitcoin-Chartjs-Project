7336ef708e63940df98c537782cb39b86ec77c23
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 13:52:01 +0200

    First test with chart

--- src/components/BitcoinChartFetch.js/BitcoinChartFetch.js
+++ src/components/BitcoinChartFetch.js/BitcoinChartFetch.js
@@ -24,5 +24,29 @@ export default function BitcoinChartFetch() {
     fetchBitcoinData();
   }, []);
 
-  return <div></div>;
+  const data = {
+    labels: dateLabel,
+    datasets: [
+      {
+        label: "Usd",
+        data: bitcoinUsd,
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
 }
