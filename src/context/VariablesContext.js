7eee7d7cfaf6ceab0dfe0d1a6a7f7f06d510d65d
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 10:29:01 +0200

    Context Provider defined

--- src/context/VariablesContext.js
+++ src/context/VariablesContext.js
@@ -6,3 +6,20 @@ const initContextVariables = {
 };
 
 export const VariablesContext = createContext(initContextVariables);
+export const VariablesContextProvider = ({ children }) => {
+  const [startDate, setStartDate] = useState(initContextVariables.startDate);
+  const [finalDate, setFinalDate] = useState(initContextVariables.finalDate);
+
+  return (
+    <VariablesContext.Provider
+      value={{
+        startDate,
+        setStartDate,
+        finalDate,
+        setFinalDate,
+      }}
+    >
+      {children}
+    </VariablesContext.Provider>
+  );
+};
