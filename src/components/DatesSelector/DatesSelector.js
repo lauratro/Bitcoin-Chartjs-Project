c8a8bd818e1747242affc0ab3c519340c0942097
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 10:55:55 +0200

    startdate input and handleChange func. defined

--- src/components/DatesSelector/DatesSelector.js
+++ src/components/DatesSelector/DatesSelector.js
@@ -4,11 +4,19 @@ import { VariablesContext } from "../../context/VariablesContext";
 export default function DatesSelector() {
   const { startDate, setStartDate, finalDate, setFinalDate } =
     useContext(VariablesContext);
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+  console.log("start", startDate);
   return (
     <div>
       <div>
         <label for="startDate">Initial Date</label>
-        <input type="date" value={startDate}></input>
+        <input
+          type="date"
+          value={startDate}
+          onChange={handleStartDateChange}
+        ></input>
       </div>
     </div>
   );
