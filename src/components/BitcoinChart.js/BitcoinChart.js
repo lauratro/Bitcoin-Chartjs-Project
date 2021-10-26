849d553632f330f00fb56b0ce3a11987ac257b84
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 11:55:17 +0200

    Fetch test in BitcoinChart component

--- src/components/BitcoinChart.js/BitcoinChart.js
+++ src/components/BitcoinChart.js/BitcoinChart.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function BitcoinChart() {
+  useEffect(() => {
+    let fetchBitcoinData = async () => {
+      try {
+        let response = await fetch(
+          "https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-10-20&end=2021-10-26"
+        );
+        let data = await response.json();
+        console.log("data", data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchBitcoinData();
+  }, []);
+
   return <div></div>;
 }
