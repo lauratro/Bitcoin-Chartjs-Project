28545d6257f853dfd85119f4a6d9c6a13832c017
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 13:44:42 +0200

    array for label and bitcoin created

--- src/components/BitcoinChartFetch.js/BitcoinChartFetch.js
+++ src/components/BitcoinChartFetch.js/BitcoinChartFetch.js
@@ -3,6 +3,8 @@ import { Line } from "react-chartjs-2";
 
 export default function BitcoinChartFetch() {
   const [chartData, setChartData] = useState({});
+  const [dateLabel, setDateLabel] = useState([]);
+  const [bitcoinUsd, setBitcoinUsd] = useState([]);
   useEffect(() => {
     let fetchBitcoinData = async () => {
       try {
@@ -10,7 +12,11 @@ export default function BitcoinChartFetch() {
           "https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-10-20&end=2021-10-22"
         );
         let data = await response.json();
-        console.log("data", data);
+        console.log("data", data.bpi);
+        let labels = await setDateLabel(Object.keys(data.bpi));
+        let bitcoin = await setBitcoinUsd(Object.values(data.bpi));
+        console.log("key", dateLabel);
+        console.log("coin", bitcoinUsd);
       } catch (err) {
         console.log(err);
       }
