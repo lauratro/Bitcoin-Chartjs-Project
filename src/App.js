8e150ed93d361f223d34556728083d93b129202f
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 11:51:56 +0200

    BitcoinChart imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import DatesSelector from "./components/DatesSelector/DatesSelector";
+import BitcoinChart from "./components/BitcoinChart.js/BitcoinChart";
 
 function App() {
   return (
     <div className="App">
       <DatesSelector />
+      <BitcoinChart />
     </div>
   );
 }
