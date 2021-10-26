70bd9649c644d5b954441bd9c7377436e504a053
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-26 15:10:01 +0200

    added h1 title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BitcoinChartFetch from "./components/BitcoinChartFetch.js/BitcoinChartFet
 function App() {
   return (
     <div className="App">
+      <h1 className="title">Bitcoin Line Chart</h1>
       <DatesSelector />
       <BitcoinChartFetch />
     </div>
