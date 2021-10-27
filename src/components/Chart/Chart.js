09b6bd9817e0db12d4c3430a28c03961b5517582
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-27 12:10:40 +0200

    chart changed to Bar

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import "./Chart.css";
 
 export default function Chart({ data, options }) {
   return (
     <div className="externalContainer">
       <div className="chartContainer">
-        <Line data={data} options={options} />
+        <Bar data={data} options={options} />
       </div>
     </div>
   );
